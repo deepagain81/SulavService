70bfcad42cfe95923c1e1f77ae3f474366210dcf
Deepak Chapagain <deepagain81@gmail.com>
2019-08-19 17:56:54 -0500

    nav-bar and routing updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {  RouterModule} from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { ProductModule } from './product/product.module';
+import { SigningModule } from './signing/signing.module';
+import { HomeComponent } from './home/home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PagenotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ProductModule,
+    SigningModule,
+    SharedModule,
+    RouterModule.forRoot([
+      {path:'home',component:HomeComponent},
+      {path:'',redirectTo:'home', pathMatch:'full'},
+      {path:'**',component:PagenotfoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
